7118e819c261f038571dc876a8eaef66e7606662
Karla Jane Jutoy <karlajanejutoy.com>
2026-05-04 00:57:05 +0800

    2 Edit view index

--- auctions/views.py
+++ auctions/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from .models import User
-
+from .models import Listing
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.filter(is_active=True)
 
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 def login_view(request):
     if request.method == "POST":
